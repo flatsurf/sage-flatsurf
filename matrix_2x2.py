f7b5adf3740f9f23ed94281da89156b39f5d195e
videlec <vincent.delecroix@labri.fr>
2013-11-12 12:04:56 -0500

    update the file matrix_2x2.py (for 2x2 matrices and planar geometry)

--- matrix_2x2.py
+++ matrix_2x2.py
@@ -1,8 +1,10 @@
 r"""
-Some tools for 2x2 matrices.
+Some tools for 2x2 matrices and planar geometry.
 """
+from sage.misc.cachefunc import cached_function
 
-from sage.rings.qqbar import AA
+from sage.rings.rational_field import QQ
+from sage.rings.qqbar import QQbar,AA
 from sage.rings.rational import Rational
 from sage.rings.real_mpfr import RR
 
@@ -11,6 +13,22 @@ from math import pi as pi_float
 from sage.symbolic.constants import pi
 from sage.matrix.constructor import matrix, identity_matrix
 
+@cached_function
+def imaginary_unit():
+    r"""
+    Return the imaginary unit as an element of QQbar.
+
+    EXAMPLES::
+
+        sage: imaginary_unit()
+        1*I
+    """
+    from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+    from sage.rings.all import CIF
+
+    R = PolynomialRing(QQ,'x')
+    x = R.gen()
+    return QQbar.polynomial_root(x**2 + 1, CIF(RIF(0,0), RIF(0.99,1.01)))
 
 def number_field_to_AA(a):
     r"""
@@ -25,6 +43,15 @@ def number_field_to_AA(a):
 def is_similarity(m):
     r"""
     Return ``True`` if ``m`` is a similarity and ``False`` otherwise.
+
+    EXAMPLES::
+
+        sage: is_similarity(matrix([[0,1],[1,0]]))
+        True
+        sage: is_similarity(matrix([[0,-2],[2,0]]))
+        True
+        sage: is_similarity(matrix([[1,1],[0,1]]))
+        False
     """
     n = m * m.transpose()
     return n[0,1] == 0 and n[1,0] == 0
@@ -43,10 +70,10 @@ def homothety_rotation_decomposition(m):
         sage: m = matrix([[sqrt2, -sqrt2],[sqrt2,sqrt2]])
         sage: a,rot = homothety_rotation_decomposition(m)
         sage: a
-        2.000000000000000?
+        2
         sage: rot
-        [ 0.7071067811865475? -0.7071067811865475?]
-        [ 0.7071067811865475?  0.7071067811865475?]
+        [ 1/2*sqrt2 -1/2*sqrt2]
+        [ 1/2*sqrt2  1/2*sqrt2]
     """
     if not is_similarity(m):
         raise ValueError("the matrix must be a similarity")
@@ -183,38 +210,124 @@ def rotation_matrix_angle(r):
 
     raise ValueError("an unexpected error occurred!")
 
-def angle(u,v):
-    r"""
-    Return the angle between the vectors ``u`` and ``v``.
 
-    This will not work if the angle is too small!!
+def is_cosine_sine_of_rational(c,s):
+    r"""
+    Check whether the given pair is a cosine and sine of a same rational angle.
+
+    EXAMPLES::
+
+        sage: c = s = AA(sqrt(2))/2
+        sage: is_cosine_sine_of_rational(c,s)
+        True
+        sage: c = AA(sqrt(3))/2; s = AA(1/2)
+        sage: is_cosine_sine_of_rational(c,s)
+        True
+
+        sage: c = AA(sqrt(5)/2); s = (1 - c**2).sqrt()
+        sage: c**2 + s**2
+        1.000000000000000?
+        sage: is_cosine_sine_of_rational(c,s)
+        False
+
+        sage: c = (AA(sqrt(5)) + 1)/4; s = (1 - c**2).sqrt()
+        sage: is_cosine_sine_of_rational(c,s)
+        True
+    """
+    zeta = QQbar(c) + imaginary_unit() * QQbar(s)
+    return zeta.minpoly().is_cyclotomic()
+
+def angle(u, v, assume_rational=False):
+    r"""
+    Return the angle between the vectors ``u`` and ``v`` divided by `2 \pi`.
+
+    INPUT:
+
+    - ``u``, ``v`` - vectors
+
+    - ``assume_rational`` - whether we assume that the angle is a multiple
+      rational of ``pi``. By default it is ``False`` but if it is known in
+      advance that the result is rational then setting it to ``True`` might be
+      much faster.
 
     EXAMPLES:
 
     As the implementation is dirty, we at least check that it works for all
-    denominator up to 100::
+    denominator up to 20::
 
         sage: u = vector((AA(1),AA(0)))
-        sage: for n in xsrange(1,100):
-        ....:     print n
+        sage: for n in xsrange(1,20):       # long time  (10 sec)
         ....:     for k in xsrange(1,n):
         ....:         v = vector((AA(cos(2*k*pi/n)), AA(sin(2*k*pi/n))))
-        ....:         assert angle(u,v) == k/n, "failed for %d/%d get angle(u,v)=%d"%(k,n,angle(u,v))
+        ....:         assert angle(u,v) == k/n
+
+    And we test up to 50 when setting ``assume_rational`` to ``True``::
+
+        sage: for n in xsrange(1,50):       # long time  (25 sec)
+        ....:     for k in xsrange(1,n):
+        ....:         v = vector((AA(cos(2*k*pi/n)), AA(sin(2*k*pi/n))))
+        ....:         assert angle(u,v,assume_rational=True) == k/n
+
+    If the angle is not rational, then the method returns an element in the real
+    lazy field::
+
+        sage: v = vector((AA(sqrt(2)), AA(sqrt(3))))
+        sage: a = angle(u,v)
+        sage: a
+        0.1410235542122437?
+        sage: exp(2*pi.n()*CC(0,1)*a.n())
+        0.632455532033676 + 0.774596669241483*I
+        sage: v / v.norm()
+        (0.6324555320336758?, 0.774596669241484?)
     """
-    # fast and dirty way
-    from math import acos,asin,sqrt,pi
+    if not assume_rational:
+        sqnorm_u = u[0] * u[0] + u[1] * u[1]
+        sqnorm_v = v[0] * v[0] + v[1] * v[1]
 
-    u0 = float(u[0]); u1 = float(u[1])
-    v0 = float(v[0]); v1 = float(v[1])
+        if sqnorm_u != sqnorm_v:
+            uu = u.change_ring(AA)
+            vv = (AA(sqnorm_u) / AA(sqnorm_v)).sqrt() * v.change_ring(AA)
+        else:
+            uu = u
+            vv= v
 
-    cos_uv = (u0*v0 + u1*v1) / sqrt((u0*u0 + u1*u1)*(v0*v0 + v1*v1))
-    angle = acos(float(cos_uv)) / (2*pi)   # rat number between 0 and 1/2
-    angle_rat = RR(angle).nearby_rational(0.000001)
-    if u0*v1 - u1*v0 < 0:
-        return 1 - angle_rat
-    return angle_rat
+        cos_uv = (uu[0]*vv[0] + uu[1]*vv[1]) / sqnorm_u
+        sin_uv = (uu[0]*vv[1] - uu[1]*vv[0]) / sqnorm_u
+
+        is_rational = is_cosine_sine_of_rational(cos_uv, sin_uv)
+
+    else:
+        is_rational = True
+
+    if is_rational:
+        # fast and dirty way using floating point approximation
+        # (see below for a slow but exact method)
+        from math import acos,asin,sqrt
+
+        u0 = float(u[0]); u1 = float(u[1])
+        v0 = float(v[0]); v1 = float(v[1])
+
+        cos_uv = (u0*v0 + u1*v1) / sqrt((u0*u0 + u1*u1)*(v0*v0 + v1*v1))
+        angle = acos(float(cos_uv)) / (2*pi_float)   # rat number between 0 and 1/2
+        angle_rat = RR(angle).nearby_rational(0.00000001)
+        if angle_rat.denominator() > 100:
+            raise NotImplementedError("the numerical method used is not smart enough!")
+        if u0*v1 - u1*v0 < 0:
+            return 1 - angle_rat
+        return angle_rat
+
+    else:
+        from sage.functions.trig import acos
+        from sage.rings.real_lazy import RLF
+        from sage.symbolic.constants import pi
+
+        if sin_uv > 0:
+            return acos(RLF(cos_uv)) / RLF(2*pi)
+        else:
+            return -acos(RLF(cos_uv)) / RLF(2*pi)
 
     # a neater way is provided below by working only with number fields
+    # but this method is slower...
     #sqnorm_u = u[0]*u[0] + u[1]*u[1]
     #sqnorm_v = v[0]*v[0] + v[1]*v[1]
     #
