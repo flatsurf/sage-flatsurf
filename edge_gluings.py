59593c5692483fb43bd437fe90795890f3ba6fbc
videlec <vincent.delecroix@labri.fr>
2013-11-28 13:52:13 +0100

    allow infinitely many labels (and not 26*2 only)

--- edge_gluings.py
+++ edge_gluings.py
@@ -1,23 +1,112 @@
 r"""
 Class to store and manipulate edge identifications between polygons.
+
+TODO:
+
+- The current structure only label external edges but it is sometimes convenient
+  to label all of them. Especially when one consider cutting sequences in
+  Bouw-Moeller lattice surfaces.
+
+- Sometimes we want labels to be fixed (for example on an origami where the
+  squares are labeld 1,2,... it is convenient to use a1, b1, a2, b2,...).
 """
 from similarity_surface import *
 
-class AbstractLabels:
-    r""" 
-    Abstract class for storing labels for edges in a similarity surface (or similar)
+lower_and_upper_case = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    The method "get_opposite" musst be overriden.
+from sage.rings.integer import Integer
+from sage.structure.unique_representation import UniqueRepresentation
+from sage.structure.parent import Parent
+class InfiniteAlphabet(Parent, UniqueRepresentation):
+    r"""
+    EXAMPLES::
+
+        sage: I = InfiniteAlphabet(); I
+        {a, b, c, ..., A, B, ..., aa, ab, ... }
+        sage: it = iter(I)
+        sage: ' '.join(it.next() for _ in xrange(26))
+        'a b c d e f g h i j k l m n o p q r s t u v w x y z'
+        sage: ' '.join(it.next() for _ in xrange(26))
+        'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
+        sage: ' '.join(it.next() for _ in xrange(26))
+        'aa ab ac ad ae af ag ah ai aj ak al am an ao ap aq ar as at au av aw ax ay az'
+        sage: for _ in xrange(2**16): it.next();
+        sage: it.next()
+        'xmQ'
     """
     def __init__(self):
-        self._labels={}
-        self._label_to_edge={}
+        from sage.categories.infinite_enumerated_sets import InfiniteEnumeratedSets
+        Parent.__init__(self, category=InfiniteEnumeratedSets())
+
+    def _repr_(self):
+        return '{a, b, c, ..., A, B, ..., aa, ab, ... }'
+
+    def __contains__(self, elt):
+        if not isinstance(elt, str):
+            return False
+        for i in elt:
+            if ord(i) < 65 or ord(i) > 122:
+                return False
+            if ord(i) > 90 and ord(i) < 97:
+                return False
+        return True
+
+    def _rank_one_letter(self, letter):
+        r"""
+        TESTS::
+
+            sage: I = InfiniteAlphabet()
+            sage: I._rank_one_letter('a')
+            0
+            sage: I._rank_one_letter('A')
+            26
+            sage: I._rank_one_letter('Z')
+            51
+        """
+        # ord('a') = 97
+        # ord('z') = 122
+        # ord('A') = 65
+        # ord('Z') = 90
+        n = ord(letter)
+        if n > 96:
+            if n > 122:
+                raise ValueError("%s not in self"%letter)
+            return n-97
+        if n < 65 or n > 90:
+            raise ValueError("%s not in self"%letter)
+        return 26+n-65
+
+    def __iter__(self):
+        from itertools import product,count
+
+        for n in count(1):
+            for word in product(lower_and_upper_case, repeat=n):
+                yield ''.join(word)
+
+class EdgeLabels:
+    r"""
+    Abstract class for storing labels for edges in a similarity surface (or similar)
+
+    The method "get_opposite" must be overriden.
+    """
+    def __init__(self, label_set=None):
+        r"""
+        INPUT:
+
+        - ``label_set`` - an optional set to be used for labels. It can be any
+          iterable (a list, a string, ...).
+        """
+        self._labels={}         # dictionnary: (p1,e1) -> label and (p2,e2) -> label
+        self._label_to_edge={}  # dictionnary: label -> (p1,e1)
+        if label_set is None:
+            label_set = InfiniteAlphabet()
+        self._label_set = label_set
 
     def get_edge(self,label):
         return self._label_to_edge.get(label)
 
     def get_opposite(self, p1, e1):
-        pass
+        raise NotImplementedError("get_opposite must be overriden in subclasses")
 
     def remove_label(self, label):
         if self._label_to_edge.has_key(label):
@@ -37,29 +126,22 @@ class AbstractLabels:
         return self._labels.get( (p1,e1) )
 
     def get_unused_label(self):
-        start=ord('a')
-        for i in range(26):
-            label=chr(start + i)
-            if not self._label_to_edge.has_key(label):
-                return label
-        start=ord('A')
-        for i in range(26):
-            label=chr(start + i)
-            if not self._label_to_edge.has_key(label):
+        for label in self._label_set:
+            if label not in self._label_to_edge:
                 return label
         raise ValueError("All available labels already used!")
 
     def add_label(self, p1, e1, label=None):
         pair2=self.get_opposite(p1,e1)
-        if pair2 is not None: 
+        if pair2 is not None:
             if label is None:
                label=self.get_unused_label()
             self._labels[(p1,e1)]=label
             self._labels[pair2]=label
             self._label_to_edge[label]=(p1,e1)
 
-class SurfaceLabels(AbstractLabels):
-    r""" 
+class SurfaceLabels(EdgeLabels):
+    r"""
     Class for storing labels for edges in a similarity surface.
     """
     def __init__(self, similarity_surface):
@@ -76,8 +158,8 @@ class SurfaceLabels(AbstractLabels):
             res=AbstractLabels.get_label(self,p1,e1)
         return res
 
-class EditableEdgeGluing(AbstractLabels):
-    r""" 
+class EditableEdgeGluing(EdgeLabels):
+    r"""
     Class for manipulating gluings and labels of a similiarity surface.
     """
     def __init__(self):
@@ -115,5 +197,4 @@ class EditableEdgeGluing(AbstractLabels):
     def glue_and_label(self, p1, e1, p2, e2, label=None):
         self.glue(p1, e1, p2, e2)
         self.add_label(p1, e1, label=label)
-
 
