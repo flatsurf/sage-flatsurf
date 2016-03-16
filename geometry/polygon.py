777c761911f5433d9460287011a8394326fc43f4
W. Patrick Hooper <wphooper@gmail.com>
2016-03-15 22:14:21 -0400

    Added method flow_to_exit based on flow which seems more useful.

--- geometry/polygon.py
+++ geometry/polygon.py
@@ -272,6 +272,79 @@ class Polygon(Element):
         # Loop terminated (on positive side of each edge)
         return PolygonPosition(PolygonPosition.INTERIOR)
 
+    def flow_to_exit(self,point,direction):
+        r"""
+        Flow a point in the direction of holonomy until the point leaves the polygon.
+        Note that ValueErrors may be thrown if the point is not in the polygon, or if 
+        it is on the boundary and the holonomy does not point into the polygon.
+
+        INPUT:
+
+        - ``point`` -- a point in the closure of the polygon (as a vector)
+
+        - ``holonomy`` -- direction of motion (a vector of non-zero length)
+
+        OUTPUT:
+
+        - The point in the boundary of the polygon where the trajectory exits
+
+        - a PolygonPosition object representing the combinatorial position of the stopping point
+        """
+        V = self.parent().vector_space()
+        if direction == V.zero():
+            raise ValueError("Zero vector provided as direction.")
+        v0=self.vertex(0)
+        w=direction
+        from sage.matrix.constructor import matrix
+        for i in range(self.num_edges()):
+            e=self.edge(i)
+            #print "i="+str(i)+" e="+str(e)+" direction="+str(direction)
+            m=matrix([[e[0], -direction[0]],[e[1], -direction[1]]])
+            try:
+                ret=m.inverse()*(point-v0)
+                s=ret[0]
+                t=ret[1]
+                #print "s="+str(s)+" and t="+str(t)
+                # What if the matrix is non-invertible?
+                
+                # Answer: You'll get a ZeroDivisionError which means that the edge is parallel
+                # to the direction.
+
+                # s is location it intersects on edge, t is the portion of the direction to reach this intersection
+                if t>0 and 0<=s and s<=1:
+                    # The ray passes through edge i.
+                    if s==1:
+                        # exits through vertex i+1
+                        v0=v0+e
+                        return v0, PolygonPosition(PolygonPosition.VERTEX, vertex= (i+1)%self.num_edges())
+                    if s==0:
+                        # exits through vertex i
+                        return v0, PolygonPosition(PolygonPosition.VERTEX, vertex= i)
+                        # exits through vertex i
+                    # exits through interior of edge i
+                    prod=t*direction
+                    return point+prod, PolygonPosition(PolygonPosition.EDGE_INTERIOR, edge=i)
+            except ZeroDivisionError:
+                # Here we know the edge and the direction are parallel
+                if wedge_product(e,point-v0)==0:
+                    # In this case point lies on the edge. 
+                    # We need to work out which direction to move in.
+                    if is_same_direction(e,point-v0):
+                        # exits through vertex i+1
+                        v0=v0+e
+                        return v0, PolygonPosition(PolygonPosition.VERTEX, vertex= (i+1)%self.num_edges())
+                    else:
+                        # exits through vertex i
+                        return v0, PolygonPosition(PolygonPosition.VERTEX, vertex= i)
+                pass
+            v0=v0+e
+        # Our loop has terminated. This can mean one of several errors...
+        pos = self.get_point_position(point,translation=translation)
+        if pos.is_outside():
+            raise ValueError("Started with point outside polygon")
+        raise ValueError("Point on boundary of polygon and direction not pointed into the polygon.")
+
+
     def flow(self,point,holonomy,translation=None):
         r"""
         Flow a point in the direction of holonomy for the length of the holonomy, or until the point leaves the polygon.
@@ -309,9 +382,9 @@ class Polygon(Element):
             return point, V.zero(), self.get_point_position(point,translation=translation)
         from sage.matrix.constructor import matrix
         if translation is None:
-            v0=V.zero()
+            v0=self.vertex(0)
         else:
-            v0=translation
+            v0=self.vertex(0)+translation
         w=holonomy
         for i in range(self.num_edges()):
             e=self.edge(i)
