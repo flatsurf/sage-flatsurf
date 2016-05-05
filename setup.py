274ab3140ce045891225b4be88599f78d68e82ad
videlec <vincent.delecroix@labri.fr>
2016-05-05 15:52:37 -0500

    extra layer

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='Flat surfaces',
+       version='0.1',
+       description='flat surfaces',
+       packages = ['flatsurf', 'flatsurf.geometry', 'flatsurf.graphical']
+       )
