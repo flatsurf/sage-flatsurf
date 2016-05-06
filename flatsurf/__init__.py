f5d8cb33234d15c4c04885028dae2cf0e75dd6ab
videlec <vincent.delecroix@labri.fr>
2016-05-06 11:14:37 -0500

    put some stuff in the __init__.py

---
+++ flatsurf/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from .geometry.polygon import polygons
+
+from .geometry.similarity_surface_generators import (similarity_surfaces,
+        translation_surfaces)
