3de038823ebfed85a9779513070cd375cb192104
Pat Hooper <wphooper@gmail.com>
2016-05-24 21:33:29 -0400

    Added surface types (e.g. TranslationSurface), Surface_polygons_and_gluings.

--- flatsurf/__init__.py
+++ flatsurf/__init__.py
@@ -4,3 +4,14 @@ from .geometry.polygon import polygons
 
 from .geometry.similarity_surface_generators import (similarity_surfaces,
         translation_surfaces)
+
+from .geometry.surface import Surface_polygons_and_gluings
+
+# The various surface types
+from .geometry.similarity_surface import SimilaritySurface
+from .geometry.half_dilation_surface import HalfDilationSurface
+from .geometry.dilation_surface import DilationSurface
+from .geometry.cone_surface import ConeSurface
+from .geometry.rational_cone_surface import RationalConeSurface
+from .geometry.half_translation_surface import HalfTranslationSurface
+from .geometry.translation_surface import TranslationSurface
