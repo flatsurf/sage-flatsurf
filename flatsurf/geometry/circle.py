e6e9fd51d2fc1907e14667bb4f312ac0b0a55602
Pat Hooper <wphooper@gmail.com>
2016-06-12 17:59:21 -0400

    Added some useful geometric methods.

--- flatsurf/geometry/circle.py
+++ flatsurf/geometry/circle.py
@@ -47,7 +47,6 @@ class Circle:
         self._center = self._V2(center)
         self._radius_squared = self._base_ring(radius_squared)
         
-        
     def center(self):
         r"""
         Return the center of the circle as a vector.
@@ -73,6 +72,63 @@ class Circle:
             return 1
         return 0
     
+    def closest_point_on_line(self, point, direction_vector):
+        r"""
+        Consider the line through the provided point in the given direction.
+        Return the closest point on this line to the center of the circle.
+        """
+        cc = self._V3((self._center[0],self._center[1], self._base_ring.one()))
+        # point at infinite orthogonal to direction_vector:
+        dd = self._V3((direction_vector[1],-direction_vector[0], self._base_ring.zero()))
+        l1 = cc.cross_product(dd)
+        
+        pp = self._V3((point[0],point[1], self._base_ring.one()))
+        # direction_vector pushed to infinity
+        ee = self._V3((direction_vector[0],direction_vector[1], self._base_ring.zero()))
+        l2 = pp.cross_product(ee)
+        
+        # This is the point we want to return
+        rr = l1.cross_product(l2)
+        try:
+            return self._V2((rr[0]/rr[2], rr[1]/rr[2]))
+        except ZeroDivisionError:
+            raise ValueError("Division by zero error. Perhaps direction is zero. "+\
+                "point="+str(point)+" direction="+str(direction_vector)+" circle="+\
+                str(self))
+
+
+    def line_position(self, point, direction_vector):
+        r"""
+        Consider the line through the provided point in the given direction.
+        We return 1 if the line passes through the circle, 0 if it is tangent
+        to the circle and -1 if the line does not intersect the circle.
+        """
+        return self.point_position(self.closest_point_on_line(point,direction_vector))
+
+    def line_segment_position(self, p, q):
+        r"""
+        Consider the open line segment pq.We return 1 if the line segment
+        enters the interior of the circle, zero if it touches the circle 
+        tangentially (at a point in the interior of the segment) and
+        and -1 if it does not touch the circle or its interior.
+        """
+        if self.point_position(p)==1:
+            return 1
+        if self.point_position(q)==1:
+            return 1
+        r=self.closest_point_on_line(p,q-p)
+        pos = self.point_position(r)
+        if pos ==-1:
+            return -1
+        # This checks if r lies in the interior of pq
+        if p[0]==q[0]:
+            if (p[1]<r[1] and r[1]<q[1]) or (p[1]>r[1] and r[1]>q[1]):
+                return pos
+        elif (p[0]<r[0] and r[0]<q[0]) or (p[0]>r[0] and r[0]>q[0]):
+            return pos
+        # It does not lie in the interior.
+        return -1
+
     def tangent_vector(self, point):
         r"""
         Return a vector based at the provided point (which must lie on the circle)
@@ -117,6 +173,31 @@ class Circle:
         rr = L.cross_product(Lperp)
         r = self._V2((rr[0]/rr[2],rr[1]/rr[2]))
         return self._V2((2*r[0]-p[0], 2*r[1]-p[1]))
+
+    def __rmul__(self, similarity):
+        r"""
+        Apply a similarity to the circle.
+        
+        EXAMPLES::
+
+            sage: from flatsurf import *
+            sage: from flatsurf.geometry.circle import *
+            sage: s=translation_surfaces.square_torus()
+            sage: c=s.polygon(0).circumscribing_circle()
+            sage: c
+            Circle((1/2, 1/2), 1/2)
+            sage: s.edge_transformation(0,2)
+            Similarity (x,y) mapsto (1*x-0*y+0, 0*x+1*y+-1)
+            sage: s.edge_transformation(0,2)*c
+            Circle((1/2, -1/2), 1/2)
+        """
+        from flatsurf.geometry.similarity import SimilarityGroup
+        SG=SimilarityGroup(self._base_ring)
+        s=SG(similarity)
+        return Circle(s(self._center), \
+            s.det()*self._radius_squared, \
+            base_ring=self._base_ring)
+
     def __str__(self):
         return "circle with center "+str(self._center)+" and radius squared " + \
             str(self._radius_squared)
