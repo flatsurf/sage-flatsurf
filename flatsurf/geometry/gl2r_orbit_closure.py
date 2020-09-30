9301f3a857633790d96365b01a80f5d4d4353e81
videlec <vincent.delecroix@labri.fr>
2020-09-30 14:14:33 +0200

    add regression test

--- flatsurf/geometry/gl2r_orbit_closure.py
+++ flatsurf/geometry/gl2r_orbit_closure.py
@@ -1032,6 +1032,33 @@ class GL2ROrbitClosure:
             sage: for d in O.decompositions(1):  # optional: pyflatsurf
             ....:     O.update_tangent_space_from_flow_decomposition(d)
             sage: assert O.dimension() == 2  # optional: pyflatsurf
+
+        TESTS:
+
+        A regression test for  https://github.com/flatsurf/sage-flatsurf/pull/69::
+
+            sage: from itertools import islice
+            sage: from flatsurf import polygons, similarity_surfaces
+            sage: from flatsurf import GL2ROrbitClosure  # optional: pyflatsurf
+
+            sage: for (a,b,c,dim) in [(3,2,2,7), (4,2,1,8), (4,4,3,11), (5,3,3,11), (5,4,4,13), (5,5,3,13)]: # long time, optional: pyflatsurf
+            ....:     T = polygons.triangle(a, b, c)
+            ....:     S = similarity_surfaces.billiard(T)
+            ....:     S = S.minimal_cover(cover_type="translation")
+            ....:     O = GL2ROrbitClosure(S)  # optional: pyflatsurf
+            ....:     nsteps = 0
+            ....:     for d in islice(O.decompositions(3,100), 10):  # optional: pyflatsurf
+            ....:         O.update_tangent_space_from_flow_decomposition(d)
+            ....:         nsteps += 1
+            ....:         if O.dimension() == dim:
+            ....:             break
+            ....:     print("(%d, %d, %d): %d" % (a, b, c, nsteps))
+            (3, 2, 2): 5
+            (4, 2, 1): 4
+            (4, 4, 3): 4
+            (5, 3, 3): 4
+            (5, 4, 4): 7
+            (5, 5, 3): 4
         """
         if self._U_rank == self._U.nrows(): return
         for v in decomposition.cylinder_deformation_subspace():
