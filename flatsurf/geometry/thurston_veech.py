8349738190c1541814b97cce54c2044b86d2d726
videlec <vincent.delecroix@labri.fr>
2020-10-16 17:17:49 +0200

    fix header

--- flatsurf/geometry/thurston_veech.py
+++ flatsurf/geometry/thurston_veech.py
@@ -20,14 +20,24 @@ REFERENCES:
 - Pascal Hubert, Erwan Lanneau
   "Veech groups without parabolic elements"
 """
-#*****************************************************************************
-#       Copyright (C) 2020 Vincent Delecroix <20100.delecroix@gmail.com>
+######################################################################
+#  This file is part of sage-flatsurf.
 #
-#  Distributed under the terms of the GNU General Public License (GPL)
-#  as published by the Free Software Foundation; either version 2 of
-#  the License, or (at your option) any later version.
-#                  https://www.gnu.org/licenses/
-#*****************************************************************************
+#        Copyright (C) 2020 Vincent Delecroix
+#
+#  sage-flatsurf is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  sage-flatsurf is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with sage-flatsurf. If not, see <https://www.gnu.org/licenses/>.
+######################################################################
 
 from sage.all import ZZ, QQ, AA, matrix, diagonal_matrix, NumberField
 from sage.rings.qqbar import do_polred
