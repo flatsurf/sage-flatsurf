c760b112f952e202f6812004a2311da5cf20fe6a
Julian Rüth <julian.rueth@fsfe.org>
2021-04-09 17:22:49 +0200

    upgrade copyrights

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'sage-flatsurf'
-copyright = u'2016, Vincent Delecroix and Pat Hooper'
+copyright = u'2016-2021, Vincent Delecroix and Pat Hooper'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
