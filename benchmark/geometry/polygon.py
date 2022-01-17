378804080bfb5037ca7a7f00ba3bb4f3e8e311a5
Julian Rüth <julian.rueth@fsfe.org>
2022-01-17 17:53:16 -0600

    Run asv benchmarks in CI

---
+++ benchmark/geometry/polygon.py
@@ -0,0 +1,28 @@
+# ********************************************************************
+#  This file is part of sage-flatsurf.
+#
+#        Copyright (C) 2022 Julian Rüth
+#
+#  sage-flatsurf is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  sage-flatsurf is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with sage-flatsurf. If not, see <https://www.gnu.org/licenses/>.
+# ********************************************************************
+import flatsurf
+
+
+def time_triangle(angles):
+    flatsurf.polygons.triangle(*angles)
+
+
+time_triangle.params = (
+    [[22, 23, 24], [26, 48, 75]],
+)
