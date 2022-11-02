0dc6c3741f992496fe701d5ec4b1a9b39e321984
Julian Rüth <julian.rueth@fsfe.org>
2022-11-03 01:53:14 +0200

    Add harmonic differential benchmarks

---
+++ benchmark/geometry/harmonic_differentials.py
@@ -0,0 +1,40 @@
+# ********************************************************************
+#  This file is part of sage-flatsurf.
+#
+#        Copyright (C) 2022 Julian Rüth
+#
+#  sage-flatsurf is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  sage-flatsurf is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with sage-flatsurf. If not, see <https://www.gnu.org/licenses/>.
+# ********************************************************************
+import flatsurf
+
+
+def time_harmonic_differential(surface):
+    surface = surface.delaunay_triangulation()
+    surface.set_immutable()
+    Ω = flatsurf.HarmonicDifferentials(surface)
+    a = flatsurf.SimplicialHomology(surface).gens()[0]
+    H = flatsurf.SimplicialCohomology(surface)
+    Ω(H({a: 1}))
+
+
+def _3413():
+    E = flatsurf.EquiangularPolygons(3, 4, 13)
+    P = E.an_element()
+    return flatsurf.similarity_surfaces.billiard(P, rational=True).minimal_cover(cover_type="translation")
+
+
+time_harmonic_differential.params = ([
+    flatsurf.translation_surfaces.torus((1, 0), (0, 1)),
+    _3413(),
+])
