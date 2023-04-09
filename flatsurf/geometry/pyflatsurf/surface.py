b5dca785d06ac23216e65a8889fc9f7d8829d5fd
Julian Rüth <julian.rueth@fsfe.org>
2023-04-09 18:16:26 +0300

    Remove duplicated code in _from_flatsurf()

--- flatsurf/geometry/pyflatsurf/surface.py
+++ flatsurf/geometry/pyflatsurf/surface.py
@@ -50,117 +50,25 @@ class Surface_pyflatsurf(Surface):
 
     @classmethod
     def _from_flatsurf(cls, surface):
-        if not surface.is_finite():
-            raise ValueError("surface must be finite to convert to pyflatsurf")
+        r"""
+        Return a :class:`Surface_pyflatsurf` built from ``surface``, i.e.,
+        represent ``surface`` in pyflatsurf wrapped as a :class:`Surface` for
+        sage-flatsurf.
 
-        if not surface.is_triangulated():
-            raise ValueError("surface must be triangulated to convert to pyflatsurf")
+        EXAMPLES::
 
-        # populate half edges and vectors
-        n = sum(surface.polygon(lab).num_edges() for lab in surface.label_iterator())
-        half_edge_labels = {}  # map: (face lab, edge num) in flatsurf -> integer
-        vec = []  # vectors
-        k = 1  # half edge label in {1, ..., n}
-        for t0, t1 in surface.edge_gluing_iterator():
-            if t0 in half_edge_labels:
-                continue
+            sage: from flatsurf import translation_surfaces
+            sage: S = translation_surfaces.square_torus().triangulate().underlying_surface()
 
-            half_edge_labels[t0] = k
-            half_edge_labels[t1] = -k
+            sage: from flatsurf.geometry.pyflatsurf.surface import Surface_pyflatsurf
+            sage: Surface_pyflatsurf._from_flatsurf(S)
 
-            f0, e0 = t0
-            p = surface.polygon(f0)
-            vec.append(p.edge(e0))
+        """
+        if isinstance(surface, Surface_pyflatsurf):
+            return surface
 
-            k += 1
+        from flatsurf.geometry.pyflatsurf_conversion import FlatTriangulationConversion
 
-        # compute vertex and face permutations
-        vp = [None] * (n + 1)  # vertex permutation
-        fp = [None] * (n + 1)  # face permutation
-        for t in surface.edge_iterator():
-            e = half_edge_labels[t]
-            j = (t[1] + 1) % surface.polygon(t[0]).num_edges()
-            fp[e] = half_edge_labels[(t[0], j)]
-            vp[fp[e]] = -e
+        to_pyflatsurf = FlatTriangulationConversion.to_pyflatsurf(surface)
 
-        def _cycle_decomposition(p):
-            n = len(p) - 1
-            assert n % 2 == 0
-            cycles = []
-            unseen = [True] * (n + 1)
-            for i in list(range(-n // 2 + 1, 0)) + list(range(1, n // 2)):
-                if unseen[i]:
-                    j = i
-                    cycle = []
-                    while unseen[j]:
-                        unseen[j] = False
-                        cycle.append(j)
-                        j = p[j]
-                    cycles.append(cycle)
-            return cycles
-
-        # convert the vp permutation into cycles
-        verts = _cycle_decomposition(vp)
-
-        # find a finite SageMath base ring that contains all the coordinates
-        base_ring = surface.base_ring()
-
-        from sage.all import AA
-
-        if base_ring is AA:
-            from sage.rings.qqbar import number_field_elements_from_algebraics
-            from itertools import chain
-
-            base_ring = number_field_elements_from_algebraics(
-                list(chain(*[list(v) for v in vec])), embedded=True
-            )[0]
-
-        from flatsurf.features import pyflatsurf_feature
-
-        pyflatsurf_feature.require()
-        from pyflatsurf.vector import Vectors
-
-        V = Vectors(base_ring)
-        vec = [V(v).vector for v in vec]
-
-        def _check_data(vp, fp, vec):
-            r"""
-            Check consistency of data
-
-            vp - vector permutation
-            fp - face permutation
-            vec - vectors of the flat structure
-            """
-            assert isinstance(vp, list)
-            assert isinstance(fp, list)
-            assert isinstance(vec, list)
-
-            n = len(vp) - 1
-
-            assert n % 2 == 0, n
-            assert len(fp) == n + 1
-            assert len(vec) == n // 2
-
-            assert vp[0] is None
-            assert fp[0] is None
-
-            for i in range(1, n // 2 + 1):
-                # check fp/vp consistency
-                assert fp[-vp[i]] == i, i
-
-                # check that each face is a triangle and that vectors sum up to zero
-                j = fp[i]
-                k = fp[j]
-                assert i != j and i != k and fp[k] == i, (i, j, k)
-                vi = vec[i - 1] if i >= 1 else -vec[-i - 1]
-                vj = vec[j - 1] if j >= 1 else -vec[-j - 1]
-                vk = vec[k - 1] if k >= 1 else -vec[-k - 1]
-                v = vi + vj + vk
-                assert v.x() == 0, v.x()
-                assert v.y() == 0, v.y()
-
-        _check_data(vp, fp, vec)
-
-        from pyflatsurf.factory import make_surface
-
-        return Surface_pyflatsurf(make_surface(verts, vec)), None
+        return Surface_pyflatsurf(to_pyflatsurf.codomain())
