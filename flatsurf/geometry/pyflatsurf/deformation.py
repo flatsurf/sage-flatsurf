4c81b1a54c46a32f0e4c54dc65f06172057ba667
Julian Rüth <julian.rueth@fsfe.org>
2023-04-11 02:27:47 +0300

    Implement mapping cohomology to pyflatsurf

--- flatsurf/geometry/pyflatsurf/deformation.py
+++ flatsurf/geometry/pyflatsurf/deformation.py
@@ -21,7 +21,26 @@ from flatsurf.geometry.deformation import Deformation
 
 class Deformation_to_pyflatsurf(Deformation):
     def __init__(self, domain, codomain, pyflatsurf_conversion):
+        self._pyflatsurf_conversion = pyflatsurf_conversion
         super().__init__(domain, codomain)
+
+    def _image_edge(self, label, edge):
+        r"""
+        EXAMPLES::
+
+            sage: from flatsurf import translation_surfaces
+            sage: from flatsurf.geometry.pyflatsurf_conversion import FlatTriangulationConversion
+            sage: S = translation_surfaces.veech_double_n_gon(5).triangulate().underlying_surface()
+            sage: deformation = S.pyflatsurf()
+            sage: deformation._image_edge(0, 0)
+            [((1, 2, 3), 0)]
+
+        """
+        half_edge = self._pyflatsurf_conversion((label, edge))
+        face = tuple(self.codomain()._flat_triangulation.face(half_edge))
+        label = type(self.codomain())._normalize_label(face)
+        edge = label.index(half_edge)
+        return [(label, edge)]
 
 
 class Deformation_from_pyflatsurf(Deformation):
