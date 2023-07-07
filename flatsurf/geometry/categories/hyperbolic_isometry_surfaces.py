0b543576f0aca83562179608a925dc29a8dcb2f6
Julian Rüth <julian.rueth@fsfe.org>
2023-07-07 19:00:26 +0300

    Add cusps() for hyperbolic surfaces

--- flatsurf/geometry/categories/hyperbolic_isometry_surfaces.py
+++ flatsurf/geometry/categories/hyperbolic_isometry_surfaces.py
@@ -6,3 +6,7 @@ class HyperbolicIsometrySurfaces(SurfaceCategory):
         from flatsurf.geometry.categories.hyperbolic_polygonal_surfaces import HyperbolicPolygonalSurfaces
 
         return [HyperbolicPolygonalSurfaces()]
+
+    class ParentMethods:
+        def cusps(self):
+            return set(vertex for vertex in self.vertices() if next(iter(vertex.representatives()))[1].is_ideal())
