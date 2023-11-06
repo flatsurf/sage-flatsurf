3e689f15e887661ec8ddd8df7a945eb2b9af548a
Julian Rüth <julian.rueth@fsfe.org>
2023-11-07 01:32:55 +0200

    Generalize Voronoi cell computations to more complex geometries

--- flatsurf/geometry/voronoi.py
+++ flatsurf/geometry/voronoi.py
@@ -1,11 +1,14 @@
 # TODO: Documentation
 # TODO: Drop all use of coordinates. Use EuclideanPolygonPoint instead of (label, coordinates)
+# TODO: SageMath already has a VoronoiDiagram so maybe not use that name. Explain that we cannot use the builtin version since it has no weights.
+
+from sage.misc.cachefunc import cached_method
 
 class VoronoiDiagram:
     r"""
     EXAMPLES::
 
-        sage: from flatsurf.geometry.voronoi import VoronoiDiagram
+        sage: from flatsurf.geometry.voronoi import VoronoiDiagram  # random output due to deprecation warnings
         sage: from flatsurf import translation_surfaces
         sage: S = translation_surfaces.regular_octagon()
         sage: center = S(0, S.polygon(0).centroid())
@@ -13,7 +16,7 @@ class VoronoiDiagram:
         sage: V.plot()
         Graphics object consisting of 43 graphics primitives
 
-        sage: def weight(center):
+        sage: def weight(label, center):
         ....:     if center == S.polygon(0).centroid():
         ....:         return QQ(center.norm().n())
         ....:     if center in S.polygon(0).vertices():
@@ -23,6 +26,31 @@ class VoronoiDiagram:
         sage: V = VoronoiDiagram(S, S.vertices().union([center]), weight=FixedVoronoiWeight(weight))
         sage: V.plot()
         Graphics object consisting of 43 graphics primitives
+
+    The same Voronoi diagram but starting from a more complicated description
+    of the octagon::
+
+        sage: from flatsurf import Polygon, translation_surfaces, polygons
+        sage: from flatsurf.geometry.voronoi import VoronoiDiagram
+        sage: S = translation_surfaces.regular_octagon()
+        sage: S = S.subdivide().codomain()
+        sage: V = VoronoiDiagram(S, S.vertices())
+        sage: V.plot()
+        Graphics object consisting of 75 graphics primitives
+
+        sage: def weight(label, center):
+        ....:     P = S.polygon(label)
+        ....:     vertex = list(P.vertices()).index(center)
+        ....:     angle = P.angle(vertex)
+        ....:     if angle == 1/8:
+        ....:         return polygons.regular_ngon(8).centroid().norm().n()
+        ....:     assert angle == 3/16, angle
+        ....:     return 1
+
+        sage: from flatsurf.geometry.voronoi import FixedVoronoiWeight
+        sage: V = VoronoiDiagram(S, S.vertices(), weight=FixedVoronoiWeight(weight))
+        sage: V.plot()
+        Graphics object consisting of 75 graphics primitives
 
     """
 
@@ -41,14 +69,17 @@ class VoronoiDiagram:
         # TODO: These mapping structures are too complex.
         self._polygon_diagrams = {label: VoronoiDiagram_Polygon(surface.polygon(label), [coordinates for point in points for (lbl, coordinates) in point.representatives() if lbl == label], create_half_space=lambda *args: weight.create_half_space(label, *args)) for label in surface.labels()}
         self._segments = {point: [
-            VoronoiCellBoundarySegment(point, surface(label, other_coordinates), label, coordinates, other_coordinates, segment) for (label, coordinates) in point.representatives() for (other_coordinates, segment) in self._polygon_diagrams[label].segments(coordinates).items()]
+            VoronoiCellBoundarySegment(point, surface(label, other_center), label, center, other_center, segment) for (label, center) in point.representatives() for (other_center, segment) in self._polygon_diagrams[label].boundary_segments(center).items()]
             for point in points}
 
-    def plot(self):
-        colors = ["red", "green"]
-        return self._surface.plot(edge_labels=False, polygon_labels=False) + \
-            sum(center.plot(color=colors[i]) for i, center in enumerate(self._segments)) + \
-            sum(segment.plot(color=colors[i]) for i, point in enumerate(self._segments) for segment in self._segments[point])
+    def plot(self, gs=None):
+        # TODO: Generalize
+        colors = ["red", "green", "orange", "purple", "cyan", "blue"]
+        if gs is None:
+            gs = self._surface.graphical_surface(edge_labels=False, polygon_labels=False)
+        return gs.plot() + \
+            sum(center.plot(gs, color=colors[i]) for i, center in enumerate(self._segments)) + \
+            sum(segment.plot(gs=gs, color=colors[i]) for i, point in enumerate(self._segments) for segment in self._segments[point])
 
     def cell(self, center):
         return self._segments[center]
@@ -68,25 +99,106 @@ class VoronoiDiagram:
 class FixedVoronoiWeight:
     def __init__(self, weight=None):
         if weight is None:
-            def weight(_): return 1
+            def weight(_, __): return 1
 
         self._weight = weight
 
     def create_half_space(self, label, center, point):
         a, b = center - point
         # TODO: Generalize this to get half spaces that are weighted by the radii of convergence.
-        weights = self._weight(point), self._weight(center)
+        weights = self._weight(label, point), self._weight(label, center)
         midpoint = (weights[1] * point + weights[0] * center) / sum(weights)
-        c = - a * midpoint[0] - b * midpoint[1]
-        return HalfSpace(a, b, c)
+        c = a * midpoint[0] + b * midpoint[1]
+
+        from sage.all import Polyhedron
+        return Polyhedron(ieqs=[(-c, a, b)])
 
 
 class VoronoiDiagram_Polygon:
-    def __init__(self, polygon, points, create_half_space):
-        self._polygon = polygon
+    r"""
+    The part of the Voronoi diagram inside ``polygon`` with cells centered at
+    ``points``.
 
-        self._half_spaces = {center: {point: create_half_space(center, point) for point in points if point != center} for center in points}
-        self._segments = {center: self._segments(self._half_spaces[center]) for center in points}
+    TESTS::
+
+        sage: from flatsurf import Polygon
+        sage: K.<a> = QuadraticField(2)
+        sage: P = Polygon(vertices=([(1, 0), (1/2*a + 1, 1/2*a), (1/2, 1/2*a + 1/2)]))
+        sage: points = list(P.vertices())
+        sage: points = points[1:] + points[:1]
+
+        sage: def weight(_, center):
+        ....:     assert center in points
+        ....:     if center == points[2]:
+        ....:         return 1/8
+        ....:     return 1
+
+        sage: from flatsurf.geometry.voronoi import VoronoiDiagram_Polygon, FixedVoronoiWeight
+        sage: VoronoiDiagram_Polygon(P, points, lambda *args: FixedVoronoiWeight(weight).create_half_space(None, *args))
+
+    """
+
+    def __init__(self, polygon, centers, create_half_space):
+        self._polygon = polygon
+        self._centers = centers
+        self._create_half_space = create_half_space
+
+        # Determine the segments that bound each Voronoi cell within this
+        # polygon, indexed by the coordinates of the center of the Voronoi
+        # cell.
+        self._cell_boundary = {}
+        for center in centers:
+            self._cell_boundary[center] = {}
+
+            # Each segment is on the boundary of one of the half spaces
+            # defining this Voronoi cell. Namely, if the half space is not
+            # trivial in the intersection, then it contributes a segment to the
+            # boundary of the cell.
+            half_spaces = list(self._boundary_half_spaces(center).values()) + self._polygon_half_spaces()
+
+            from itertools import chain
+            from sage.all import Polyhedron
+            intersection = Polyhedron(ieqs=chain.from_iterable(half_space.inequalities() for half_space in half_spaces))
+
+            assert not intersection.is_empty()
+            assert intersection.dimension() == 2
+
+            segments = intersection.faces(1)
+            assert all(segment.is_compact() for segment in segments)
+
+            segments = [Polyhedron(vertices=segment.vertices()) for segment in segments]
+
+            segments = [segment for segment in segments if center not in segment]
+
+            assert segments
+
+            # Associate with each segment which pair of vertices produced it,
+            # i.e., apart from center which other point.
+            for segment in segments:
+                opposite_centers = [opposite_center for opposite_center, half_space in self._boundary_half_spaces(center).items() if segment.intersection(half_space.faces(1)[0].as_polyhedron()) == segment]
+                assert len(opposite_centers) == 1, "segment must be induced by exactly one other center of a Voronoi cell"
+
+                opposite_center = opposite_centers[0]
+                assert opposite_center not in self._cell_boundary[center]
+
+                self._cell_boundary[center][opposite_center] = segment
+
+    @cached_method
+    def _boundary_half_spaces(self, center):
+        r"""
+        Return half spaces whose intersection is the Voronoi cell at
+        ``center`` one for each other center of a Voronoi cell.
+        """
+        return {opposite_center: self._create_half_space(center, opposite_center) for opposite_center in self._centers if opposite_center != center}
+
+    @cached_method
+    def _polygon_half_spaces(self):
+        r"""
+        Return the half spaces whose intersection is the underlying polygon.
+        """
+        # TODO: Implement this in polygon?
+        from sage.all import Polyhedron
+        return [Polyhedron(ieqs=[(vertex[0] * edge[1] - vertex[1] * edge[0], -edge[1], edge[0])]) for (vertex, edge) in zip(self._polygon.vertices(), self._polygon.edges())]
 
     def relativize(self, coordinates):
         r"""
@@ -98,22 +210,12 @@ class VoronoiDiagram_Polygon:
 
         assert False
 
-    def segments(self, coordinates):
-        return self._segments[coordinates]
-
-    def _segments(self, half_spaces):
-        segments = {}
-        for point, half_space in half_spaces.items():
-            from itertools import chain
-            endpoints = set(
-                list(chain.from_iterable(half_space.half_space_boundary_intersections(other) for other in half_spaces.values() if other is not half_space)) +
-                list(half_space.polygon_boundary_intersection(self._polygon))
-            )
-            endpoints = [endpoint for endpoint in endpoints if self._polygon.contains_point(endpoint) and all(half_space.contains(endpoint) for half_space in half_spaces.values())]
-            if endpoints:
-                segments[point] = Segment(half_space, endpoints)
-
-        return segments
+    def boundary_segments(self, center):
+        r"""
+        Return the segments bounding the Voronoi cell centered at the
+        coordinates ``center`` in this polygon.
+        """
+        return self._cell_boundary[center]
 
 
 class VoronoiCellBoundarySegment:
@@ -125,8 +227,13 @@ class VoronoiCellBoundarySegment:
         self._other_coordinates = other_coordinates
         self._segment = segment
 
-    def plot(self, **kwargs):
-        return self._segment.plot(**kwargs)
+    def plot(self, gs=None, **kwargs):
+        shift = None
+        if gs is not None:
+            graphical_polygon = gs.graphical_polygon(self._label)
+            polygon = graphical_polygon.base_polygon()
+            shift = graphical_polygon.transformed_vertex(0) - polygon.vertex(0)
+        return (self._segment + shift).plot(point=False, **kwargs)
 
     def center(self):
         return self._center, self._label, self._coordinates
@@ -165,6 +272,7 @@ class VoronoiCellBoundarySegment:
         return VoronoiCellBoundarySegment(self._other, self._center, self._label, self._other_coordinates, self._coordinates, -self._segment)
 
 
+# TODO: Deleteme
 class HalfSpace:
     r"""
     The half space ax + by + c ≥ 0.
@@ -272,9 +380,10 @@ class Segment:
         self._half_space = half_space
         self._endpoints = endpoints
 
-    def plot(self, **kwargs):
+    def plot(self, shift=None, **kwargs):
         from sage.all import arrow
-        return arrow(*self._endpoints, arrowsize=3, **kwargs)
+        endpoints = [e + shift for e in self._endpoints]
+        return arrow(*endpoints, arrowsize=3, **kwargs)
 
     def split_vertically(self, y):
         if (self._endpoints[0][1] < y and self._endpoints[1][1] > y) or (self._endpoints[0][1] > y and self._endpoints[1][1] < y):
