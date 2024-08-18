7a714000640cb6a67d272358fa5b61a9cd309d48
Julian Rüth <julian.rueth@fsfe.org>
2024-08-18 16:32:29 +0300

    Document Surface_pyflatsurf

--- flatsurf/geometry/pyflatsurf/surface.py
+++ flatsurf/geometry/pyflatsurf/surface.py
@@ -1,8 +1,57 @@
+r"""
+Surfaces backed by pyflatsurf.
+
+There should be no need to create such surfaces directly, not even for authors
+of sage-flatsurf. Instead just call :meth:`pyflatsurf` on a surface which
+returns such a surface (or rather, a morphism to such a surface.)
+
+EXAMPLES::
+
+    sage: from flatsurf import translation_surfaces
+    sage: S = translation_surfaces.square_torus()
+
+    sage: T = S.pyflatsurf().codomain()
+    sage: T
+    Surface backed by FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2), faces = (1, 2, 3)(-1, -2, -3)) with vectors {1: (1, 0), 2: (0, 1), 3: (-1, -1)}
+
+Ideally, there should be no need to use the underlying ``FlatTriangulation``
+directly, but it can be accessed with :meth:`flat_triangulation`::
+
+    sage: T.flat_triangulation()
+    FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2), faces = (1, 2, 3)(-1, -2, -3)) with vectors {1: (1, 0), 2: (0, 1), 3: (-1, -1)}
+
+"""
+# ********************************************************************
+#  This file is part of sage-flatsurf.
+#
+#        Copyright (C) 2024 Julian Rüth
+#
+#  sage-flatsurf is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  sage-flatsurf is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with sage-flatsurf. If not, see <https://www.gnu.org/licenses/>.
+# ********************************************************************
 from flatsurf.geometry.surface import OrientedSimilaritySurface
+
+from sage.misc.cachefunc import cached_method
 
 
 class Surface_pyflatsurf(OrientedSimilaritySurface):
     r"""
+    A translation surface backed by pyflatsurf.
+
+    Most surfaces in sage-flatsurf, such as the
+    :class:`MutableOrientedSimilaritySurface` are implemented in Python
+    instead.
+
     EXAMPLES::
 
         sage: from flatsurf import Polygon, MutableOrientedSimilaritySurface
@@ -29,10 +78,34 @@ class Surface_pyflatsurf(OrientedSimilaritySurface):
         True
         sage: TestSuite(T).run()
 
+    Disconnected surfaces can be import from pyflatsurf::
+
+        sage: S = MutableOrientedSimilaritySurface(QQ)
+        sage: S.add_polygon(Polygon(vertices=[(0, 0), (1, 0), (1, 1), (0, 1)]))
+        0
+        sage: S.add_polygon(Polygon(vertices=[(0, 0), (1, 0), (1, 1), (0, 1)]))
+        1
+
+        sage: S.glue((0, 0), (0, 2))
+        sage: S.glue((0, 1), (0, 3))
+        sage: S.glue((1, 0), (1, 2))
+        sage: S.glue((1, 1), (1, 3))
+
+        sage: S.set_immutable()
+
+        sage: T = S.pyflatsurf().codomain()
+
+        sage: T.is_connected()
+        False
+
+        sage: T.flat_triangulation()
+        FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2)(4, -6, 5, -4, 6, -5), faces = (1, 2, 3)(-1, -2, -3)(4, 5, 6)(-4, -5, -6)) with vectors {1: (1, 0), 2: (0, 1), 3: (-1, -1), 4: (1, 0), 5: (0, 1), 6: (-1, -1)}
+
+        sage: TestSuite(T).run()
+
     """
 
     def __init__(self, flat_triangulation):
-        # TODO: Check that _flat_triangulation is never used by _flat_triangulation instead.
         self._flat_triangulation = flat_triangulation
 
         from flatsurf.geometry.pyflatsurf_conversion import RingConversion
@@ -49,35 +122,109 @@ class Surface_pyflatsurf(OrientedSimilaritySurface):
 
         from flatsurf.geometry.categories import TranslationSurfaces
 
-        # TODO: This is assuming that the surface is connected. Currently that's the case for all surfaces in libflatsurf?
-        category = TranslationSurfaces().FiniteType().Connected()
+        category = TranslationSurfaces().FiniteType()
         if flat_triangulation.hasBoundary():
             category = category.WithBoundary()
         else:
             category = category.WithoutBoundary()
+
         super().__init__(base=self._ring_conversion.domain(), category=category)
 
-    def __eq__(self, other):
-        if not isinstance(other, Surface_pyflatsurf):
-            return False
-
-        return self._flat_triangulation == other._flat_triangulation
-
-    def __hash__(self):
-        # TODO: This is not working. Flat triangulations that are equal do not produce the same hashes.
-        return hash(self._flat_triangulation)
+        if self.is_connected():
+            self._refine_category_(category.Connected())
 
     def flat_triangulation(self):
+        r"""
+        Return the pyflatsurf ``FlatTriangulation`` object underlying this
+        surface.
+
+        .. WARNING::
+
+            This surface is supposed to be immutable. However, the
+            ``FlatTriangulation`` is not immutable. If you make any changes to
+            it, things might break in strange ways. If you want to make
+            modifications to the ``FlatTriangulation`` be sure to work on a
+            ``clone()`` of it instead.
+
+        EXAMPLES::
+
+            sage: from flatsurf import translation_surfaces
+            sage: S = translation_surfaces.square_torus()
+            sage: S.pyflatsurf().codomain().flat_triangulation()
+            FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2), faces = (1, 2, 3)(-1, -2, -3)) with vectors {1: (1, 0), 2: (0, 1), 3: (-1, -1)}
+
+        """
         return self._flat_triangulation
 
     def is_mutable(self):
+        r"""
+        Return whether this surface is mutable.
+
+        .. WARNING::
+
+            This surface is supposed to be immutable. However, the underlying
+            ``FlatTriangulation`` is not immutable. If you make any changes to
+            it, things might break in strange ways. If you want to make
+            modifications to the ``FlatTriangulation`` be sure to work on a
+            ``clone()`` of it instead. See :meth:`flat_triangulation`.
+
+        EXAMPLES::
+
+            sage: from flatsurf import translation_surfaces
+            sage: S = translation_surfaces.square_torus()
+            sage: S.pyflatsurf().codomain().is_mutable()
+            False
+
+        """
         return False
 
+    @cached_method
     def roots(self):
-        # TODO: This is assuming that the surface is connected. Currently that's the case for all surfaces in libflatsurf?
-        return [self._normalize_label(self._flat_triangulation.face(1))]
+        r"""
+        Return some root labels for this surface, one for each connected
+        component of the surface.
+
+        EXAMPLES::
+
+            sage: from flatsurf import translation_surfaces
+            sage: S = translation_surfaces.square_torus()
+            sage: T = S.pyflatsurf().codomain()
+
+            sage: list(T.labels())
+            [(1, 2, 3), (-3, -1, -2)]
+
+            sage: T.roots()
+            ((1, 2, 3),)
+
+        """
+        roots = []
+        seen = set()
+
+        for face in self.flat_triangulation().faces():
+            root = self._normalize_label(face)
+            if root not in seen:
+                roots.append(root)
+                for root in self.component(root):
+                    seen.add(root)
+
+        return tuple(roots)
 
     def pyflatsurf(self):
+        r"""
+        Return a version of this surface that is backed by pyflatsurf. Since
+        this surface is already backed by pyflatsurf, this returns a trivial
+        morphism.
+
+        EXAMPLES::
+
+            sage: from flatsurf import translation_surfaces
+            sage: S = translation_surfaces.square_torus()
+            sage: T = S.pyflatsurf().codomain()
+
+            sage: T.pyflatsurf()
+            Identity endomorphism of Surface backed by FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2), faces = (1, 2, 3)(-1, -2, -3)) with vectors {1: (1, 0), 2: (0, 1), 3: (-1, -1)}
+
+        """
         from flatsurf.geometry.morphism import IdentityMorphism
 
         return IdentityMorphism._create_morphism(self)
@@ -96,9 +243,9 @@ class Surface_pyflatsurf(OrientedSimilaritySurface):
 
             sage: from flatsurf.geometry.pyflatsurf.surface import Surface_pyflatsurf
             sage: Surface_pyflatsurf._from_flatsurf(S)
-            Generic morphism:
+            pyflatsurf conversion morphism:
               From: Triangulation of Translation Surface in H_1(0) built from a square
-              To:   FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2), faces = (1, 2, 3)(-1, -2, -3)) with vectors {1: (1, 0), 2: (0, 1), 3: (-1, -1)}
+              To:   Surface backed by FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2), faces = (1, 2, 3)(-1, -2, -3)) with vectors {1: (1, 0), 2: (0, 1), 3: (-1, -1)}
 
         """
         if isinstance(surface, Surface_pyflatsurf):
@@ -124,7 +271,7 @@ class Surface_pyflatsurf(OrientedSimilaritySurface):
     def __repr__(self):
         r"""
         Return a printable representation of this surface, namely, print this
-        surface as pyflatsurf would.
+        surface essentially as pyflatsurf would.
 
         EXAMPLES::
 
@@ -133,27 +280,58 @@ class Surface_pyflatsurf(OrientedSimilaritySurface):
 
             sage: from flatsurf.geometry.pyflatsurf.surface import Surface_pyflatsurf
             sage: S.pyflatsurf().codomain()
-            FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2), faces = (1, 2, 3)(-1, -2, -3)) with vectors {1: (1, 0), 2: (0, 1), 3: (-1, -1)}
+            Surface backed by FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2), faces = (1, 2, 3)(-1, -2, -3)) with vectors {1: (1, 0), 2: (0, 1), 3: (-1, -1)}
 
         """
-        return repr(self._flat_triangulation)
+        return f"Surface backed by {self._flat_triangulation!r}"
 
-    def apply_matrix(self, m):
+    def apply_matrix(self, m, in_place=None):
+        r"""
+        Overrides the generic
+        :meth:`SimilaritySurfaces.ParentMethods.apply_matrix` for this
+        pyflatsurf backed surface.
+
+        EXAMPLES::
+
+            sage: from flatsurf import translation_surfaces
+            sage: S = translation_surfaces.square_torus().pyflatsurf().codomain()
+
+            sage: S.apply_matrix(matrix([[1, 2], [0, 1]])).codomain()
+            Surface backed by FlatTriangulationCombinatorial(vertices = (1, -3, 2, -1, 3, -2), faces = (1, 2, 3)(-1, -2, -3)) with vectors {1: (1, 0), 2: (2, 1), 3: (-3, -1)}
+
+        """
+        if in_place:
+            return super().apply_matrix(m, in_place=in_place)
+
         from sage.all import matrix
-
         m = matrix(m, ring=self.base_ring())
-
         m = [self._ring_conversion(x) for x in m.list()]
 
         deformation = self._flat_triangulation.applyMatrix(*m)
         codomain = Surface_pyflatsurf(deformation.codomain())
 
-        from flatsurf.geometry.pyflatsurf.deformation import Deformation_pyflatsurf
-
-        return Deformation_pyflatsurf(self, codomain, deformation)
+        from flatsurf.geometry.pyflatsurf.morphism import Morphism_Deformation
+        return Morphism_Deformation._create_morphism(self, codomain, deformation)
 
     @classmethod
     def _normalize_label(cls, label):
+        r"""
+        Return a normalized version of the ``label`` of this surface.
+
+        Labels are formed by the triple of the attached half edges in order.
+        They are normalized such that the minimal half edge is first.
+
+        EXAMPLES::
+
+            sage: from flatsurf.geometry.pyflatsurf.surface import Surface_pyflatsurf
+            sage: Surface_pyflatsurf._normalize_label((1, 2, 3))
+            (1, 2, 3)
+            sage: Surface_pyflatsurf._normalize_label((1, -2, 3))
+            (-2, 3, 1)
+            sage: Surface_pyflatsurf._normalize_label((1, -2, -3))
+            (-3, 1, -2)
+
+        """
         label = tuple(edge.id() if hasattr(edge, "id") else int(edge) for edge in label)
 
         shift = label.index(min(*label))
@@ -220,44 +398,36 @@ class Surface_pyflatsurf(OrientedSimilaritySurface):
 
         return opposite_label, opposite_label.index(opposite_half_edge.id())
 
-    def flow_decomposition(self, direction):
-        direction = self._vector_space_conversion.domain()(direction)
-        direction = self._vector_space_conversion(direction)
+    def __eq__(self, other):
+        r"""
+        Return whether this surface is indistinguishable from ``other``.
 
-        import pyflatsurf
+        EXAMPLES::
 
-        decomposition = pyflatsurf.flatsurf.makeFlowDecomposition(
-            self._flat_triangulation,
-            direction,
-        )
+            sage: from flatsurf import translation_surfaces
+            sage: S = translation_surfaces.square_torus().pyflatsurf().codomain()
+            sage: T = translation_surfaces.square_torus().pyflatsurf().codomain()
+            sage: S == T
+            True
 
-        from flatsurf.geometry.pyflatsurf.flow_decomposition import (
-            FlowDecomposition_pyflatsurf,
-        )
+        """
+        if not isinstance(other, Surface_pyflatsurf):
+            return False
 
-        return FlowDecomposition_pyflatsurf(decomposition, surface=self)
+        return self._flat_triangulation == other._flat_triangulation
 
-    def _flow_decompositions_slopes_bfs(self, bound):
-        return self._flow_decompositions_slopes_from_connections(
-            self._flat_triangulation.connections().bound(int(bound)).byLength()
-        )
+    def __hash__(self):
+        r"""
+        Return a hash value for this surface that is compatible with
+        :meth:`__eq__`.
 
-    def _flow_decompositions_slopes_dfs(self, bound):
-        return self._flow_decompositions_slopes_from_connections(
-            self._flat_triangulation.connections().bound(int(bound))
-        )
+        EXAMPLES::
 
-    def _flow_decompositions_slopes_from_connections(self, connections):
-        import cppyy
+            sage: from flatsurf import translation_surfaces
+            sage: S = translation_surfaces.square_torus().pyflatsurf().codomain()
+            sage: T = translation_surfaces.square_torus().pyflatsurf().codomain()
+            sage: hash(S) == hash(T)
+            True
 
-        slopes = cppyy.gbl.std.set[
-            self._vector_space_conversion.codomain(),
-            self._vector_space_conversion.codomain().CompareSlope,
-        ]()
-
-        for connection in connections:
-            slope = connection.vector()
-            if slopes.find(slope) != slopes.end():
-                continue
-            slopes.insert(slope)
-            yield self._vector_space_conversion.section(slope)
+        """
+        return hash(self._flat_triangulation)
