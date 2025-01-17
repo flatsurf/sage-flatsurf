f3ddeb2c5545f04a496cfbcdc22690b1130ef740
Julian Rüth <julian.rueth@fsfe.org>
2025-01-17 20:53:59 +0200

    Force pixi 0.39.5 in installer

--- installer/unix/.ensure-pixi.sh
+++ installer/unix/.ensure-pixi.sh
@@ -9,5 +9,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PIXI="${SCRIPT_DIR}/.pixi-home/bin/pixi"
 
 if [[ ! -x "$PIXI" ]]; then
-  PIXI_HOME="${SCRIPT_DIR}/.pixi-home" PIXI_NO_PATH_UPDATE="yes" "${SCRIPT_DIR}"/.pixi-install.sh
+  # We force a version of pixi that we used in our CI, see test.yml
+  PIXI_VERSION="v0.39.5" PIXI_HOME="${SCRIPT_DIR}/.pixi-home" PIXI_NO_PATH_UPDATE="yes" "${SCRIPT_DIR}"/.pixi-install.sh
 fi
